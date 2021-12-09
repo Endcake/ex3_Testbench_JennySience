f971593583dd5ab51c1161bcf4477736bde64586
Christopher Kren <christopher.kren@gmail.com>
2021-12-09 17:30:26 +0100

    Läuft mit Server HErcules

--- JennySienceTestbench/JennySienceTestbench/Form1.Designer.cs
+++ JennySienceTestbench/JennySienceTestbench/Form1.Designer.cs
@@ -30,6 +30,12 @@
         {
             this.txtBx_host = new System.Windows.Forms.TextBox();
             this.txtBx_port = new System.Windows.Forms.TextBox();
+            this.btn_connect = new System.Windows.Forms.Button();
+            this.txtBox_communication = new System.Windows.Forms.TextBox();
+            this.btn_exit = new System.Windows.Forms.Button();
+            this.btn_disconnect = new System.Windows.Forms.Button();
+            this.txtBx_command = new System.Windows.Forms.TextBox();
+            this.btn_send_message = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // txtBx_host
@@ -46,11 +52,72 @@
             this.txtBx_port.Size = new System.Drawing.Size(100, 20);
             this.txtBx_port.TabIndex = 1;
             // 
+            // btn_connect
+            // 
+            this.btn_connect.Location = new System.Drawing.Point(352, 28);
+            this.btn_connect.Name = "btn_connect";
+            this.btn_connect.Size = new System.Drawing.Size(75, 23);
+            this.btn_connect.TabIndex = 2;
+            this.btn_connect.Text = "connect";
+            this.btn_connect.UseVisualStyleBackColor = true;
+            this.btn_connect.Click += new System.EventHandler(this.btn_connect_Click);
+            // 
+            // txtBox_communication
+            // 
+            this.txtBox_communication.Location = new System.Drawing.Point(62, 115);
+            this.txtBox_communication.Multiline = true;
+            this.txtBox_communication.Name = "txtBox_communication";
+            this.txtBox_communication.Size = new System.Drawing.Size(403, 228);
+            this.txtBox_communication.TabIndex = 3;
+            // 
+            // btn_exit
+            // 
+            this.btn_exit.Location = new System.Drawing.Point(62, 394);
+            this.btn_exit.Name = "btn_exit";
+            this.btn_exit.Size = new System.Drawing.Size(376, 23);
+            this.btn_exit.TabIndex = 4;
+            this.btn_exit.Text = "exit";
+            this.btn_exit.UseVisualStyleBackColor = true;
+            this.btn_exit.Click += new System.EventHandler(this.btn_exit_Click);
+            // 
+            // btn_disconnect
+            // 
+            this.btn_disconnect.Location = new System.Drawing.Point(489, 28);
+            this.btn_disconnect.Name = "btn_disconnect";
+            this.btn_disconnect.Size = new System.Drawing.Size(75, 23);
+            this.btn_disconnect.TabIndex = 5;
+            this.btn_disconnect.Text = "disconnect";
+            this.btn_disconnect.UseVisualStyleBackColor = true;
+            this.btn_disconnect.Click += new System.EventHandler(this.btn_disconnect_Click);
+            // 
+            // txtBx_command
+            // 
+            this.txtBx_command.Location = new System.Drawing.Point(489, 115);
+            this.txtBx_command.Name = "txtBx_command";
+            this.txtBx_command.Size = new System.Drawing.Size(100, 20);
+            this.txtBx_command.TabIndex = 6;
+            // 
+            // btn_send_message
+            // 
+            this.btn_send_message.Location = new System.Drawing.Point(596, 115);
+            this.btn_send_message.Name = "btn_send_message";
+            this.btn_send_message.Size = new System.Drawing.Size(75, 23);
+            this.btn_send_message.TabIndex = 7;
+            this.btn_send_message.Text = "send";
+            this.btn_send_message.UseVisualStyleBackColor = true;
+            this.btn_send_message.Click += new System.EventHandler(this.btn_send_message_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_send_message);
+            this.Controls.Add(this.txtBx_command);
+            this.Controls.Add(this.btn_disconnect);
+            this.Controls.Add(this.btn_exit);
+            this.Controls.Add(this.txtBox_communication);
+            this.Controls.Add(this.btn_connect);
             this.Controls.Add(this.txtBx_port);
             this.Controls.Add(this.txtBx_host);
             this.Name = "Form1";
@@ -64,6 +131,12 @@
 
         private System.Windows.Forms.TextBox txtBx_host;
         private System.Windows.Forms.TextBox txtBx_port;
+        private System.Windows.Forms.Button btn_connect;
+        private System.Windows.Forms.TextBox txtBox_communication;
+        private System.Windows.Forms.Button btn_exit;
+        private System.Windows.Forms.Button btn_disconnect;
+        private System.Windows.Forms.TextBox txtBx_command;
+        private System.Windows.Forms.Button btn_send_message;
     }
 }
 
--- JennySienceTestbench/JennySienceTestbench/Form1.cs
+++ JennySienceTestbench/JennySienceTestbench/Form1.cs
@@ -7,14 +7,67 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net.Sockets;
+using System.IO;
 
 namespace JennySienceTestbench
 {
     public partial class Form1 : Form
     {
+        TcpClient client;
+        
+        
+
+        NetworkStream stream;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btn_connect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                client= new TcpClient("127.0.0.1", 1302);
+                
+                
+                
+
+                stream = client.GetStream();
+
+
+            }
+            catch
+            {
+                MessageBox.Show("keine Verbindung zum Server");
+            }
+        }
+
+        private void btn_exit_Click(object sender, EventArgs e)
+        {
+            try { client.Close(); }
+            catch { }
+            Application.Exit();
+        }
+
+        private void btn_disconnect_Click(object sender, EventArgs e)
+        {
+            client.Close();
+        }
+
+        private void btn_send_message_Click(object sender, EventArgs e)
+        {
+            string messageToSend = txtBx_command.Text;
+            int byteCount = Encoding.ASCII.GetByteCount(messageToSend + 1);
+            byte[] sendData = new byte[byteCount];
+            sendData = Encoding.ASCII.GetBytes(messageToSend);
+
+            stream.Write(sendData, 0, sendData.Length);
+            txtBox_communication.Text = "sending data...\n";
+
+            StreamReader sr = new StreamReader(stream);
+            string response = sr.ReadLine();
+            txtBox_communication.Text = response;
         }
     }
 }
